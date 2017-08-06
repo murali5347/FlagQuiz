2ebc909d837669ba1073c45ace725ea83aa9cd72
Murali <muralivijaya547@gmail.com>
2017-08-06 10:23:04 -0400

    example deleted variables

--- FlagGame/app/src/main/java/com/example/murali/flaggame/MainActivity.java
+++ FlagGame/app/src/main/java/com/example/murali/flaggame/MainActivity.java
@@ -20,12 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String CHOICES = "pre_numberOfChoices";
     public  static  final String REGIONS = "pref_regionsToInclude";
-    public  static  final String example= "nextChange";
-     public  static  final String example1= "nextChange";
-     public  static  final String example2= "nextChange";
-     public  static  final String example3= "nextChange";
-     public  static  final String example4= "nextChange";
-     public  static  final String example5= "nextChange";
+   
     
     
     private boolean phoneDevice = true;
